a637992c55c58399fed6ba22ad5153a38a4676aa
qwganker <448081525@qq.com>
2021-01-08 14:10:38 +0800

    update

--- web/msync/src/layouts/index.vue
+++ web/msync/src/layouts/index.vue
@@ -1,11 +1,18 @@
 <template>
   <a-layout style="height: 100%;">
-    <a-layout-sider>
-              <div class="logo" />
-
-      <a-menu style="height: 100%;" theme="dark" mode="inline" :default-selected-keys="['jianshu']" :default-open-keys="['blogadmin', 'writeadmin']">
+    <a-layout-sider :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }">
+      <div class="logo" />
+      <a-menu
+        theme="dark"
+        mode="inline"
+        :default-selected-keys="['jianshu']"
+        :default-open-keys="['blogadmin', 'writeadmin']"
+      >
+      <!-- <div class="logo" /> -->
         <a-sub-menu key="blogadmin">
-          <span slot="title"><a-icon type="database" /><span>文章管理</span></span>
+          <span slot="title"
+            ><a-icon type="database" /><span>文章管理</span></span
+          >
           <a-menu-item key="jianshu">
             <router-link to="/blogadmin/jianshu" />
             简书
@@ -23,12 +30,13 @@
             开源中国
           </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="writeadmin">
-          <span slot="title"><a-icon type="highlight" /><span>创作中心</span></span>
-        </a-sub-menu>
+        <a-menu-item key="writeadmin">
+          <router-link to="/writeadmin" />
+          <a-icon type="highlight" /><span>创作中心</span>
+        </a-menu-item>
       </a-menu>
     </a-layout-sider>
-    <a-layout>
+    <a-layout :style="{ marginLeft: '200px' }">
       <router-view></router-view>
     </a-layout>
   </a-layout>
--- web/msync/src/pages/blogadmin/oschina/index.vue
+++ web/msync/src/pages/blogadmin/oschina/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    oschina
+    开发中
   </div>
 </template>
 
